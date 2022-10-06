cd6fe2bdc92caa6d92de5347a29587f2bb89f31f
Manuel <manuel.schwarz1@students.fhnw.ch>
2022-10-06 17:41:18 +0200

    images and compose file

---
+++ images/image_consumer1/data_consumer1.py
@@ -0,0 +1,54 @@
+from kafka import KafkaConsumer, KafkaProducer
+import json
+import uuid
+import pandas as pd
+import csv
+import time
+
+server1 = 'broker1:9093'
+server2 = 'broker1:9095'
+server3 = 'broker1:9097'
+
+topic1 = "data_gen1"
+topic2 = "data_gen2"
+
+consumer1 = KafkaConsumer(topic1,
+                         auto_offset_reset='earliest',
+                         bootstrap_servers=[server1],
+                         api_version=(0, 10),
+                         value_deserializer = json.loads,
+                         consumer_timeout_ms=400,
+                         client_id='joe')
+
+
+def consume_1(consumer, topic_name):
+    # get data from Kafka
+    messages = []
+    for msg in consumer:
+        #print(msg.key.decode("utf-8"), msg.value)
+        messages.append(msg.value)
+    print(f'{len(messages)} messages received on topic: {topic_name}')
+
+    # save received data to csv
+    if len(messages) != 0:
+        # write new file
+        if messages[0]['index'] == 0:
+            df_consumer1 = pd.DataFrame(messages)
+            df_consumer1.to_csv('data/consum_sinus_data.csv', mode='w', index=False)
+            print('messages saved in new file')
+        # append to file
+        else:
+            df_consumer1 = pd.DataFrame(messages)
+            df_consumer1.to_csv('data/consum_sinus_data.csv', mode='a', header=False, index=False)
+            print('messages saved to file')
+        return True
+    else:
+        print('no new messages - producer has stopped')
+        return False
+
+
+while True:
+    new_data = consume_1(consumer1, topic1)
+    if not new_data:
+        break
+    time.sleep(5)
---
+++ images/image_consumer2b/data_consumer2b.py
@@ -0,0 +1,44 @@
+from kafka import KafkaConsumer, KafkaProducer
+import json
+import uuid
+import pandas as pd
+import csv
+import time
+
+server1 = 'broker1:9093'
+server2 = 'broker2:9095'
+server3 = 'broker3:9097'
+
+topic1 = "data_gen1"
+topic2 = "data_gen2"
+
+consumer2b = KafkaConsumer(topic2,
+                         auto_offset_reset='earliest',
+                         bootstrap_servers=[server2],
+                         api_version=(0, 10),
+                         value_deserializer = json.loads,
+                         consumer_timeout_ms=1000)
+
+def consume_2(consumer, topic_name):
+    # get data from Kafka
+    messages = []
+    for msg in consumer:
+        #print(msg.key.decode("utf-8"), msg.value)
+        messages.append(msg.value)
+    print(f'{len(messages)} messages received on topic: {topic_name}')
+
+    # save received data to csv
+    if len(messages) != 0:
+        # print  latest received data point
+        print(f'latest data: {messages[-1]["data"]}')
+        return True
+    else:
+        print('no new messages - producer has stopped')
+        return False
+
+while True:
+    new_data = consume_2(consumer2b, topic2)
+    if not new_data:
+        break
+    time.sleep(10)
+
